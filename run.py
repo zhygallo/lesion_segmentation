57101f6a35a3b2f5853e0c09d84b5cb96a54ec1e
zhygallo <zhigallandrejj@gmail.com>
2018-04-23 12:02:30 +0200

    Add click commands

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import click
 import numpy as np
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
@@ -9,30 +10,31 @@ from models.VNet import get_model
 from losses import dice_coef_loss
 from metrics import dice_coef, recall, f1_score
 
-def main():
+@click.command()
+@click.argument('images_numpy_file')
+@click.argument('masks_numpy_file')
+@click.argument('outdir')
+def main(images_numpy_file, masks_numpy_file, outdir):
     crop_shape = (128, 128, 64)
     batch_size = 4
     epochs = 40
     learning_rate = 1e-5
 
-    data = np.load('numpy_data/images.npy')
+    data = np.load(images_numpy_file)
     data = data.astype('float32')
     mean = np.mean(data)  # mean for data centering
     std = np.std(data)  # std for data normalization
     data -= mean
     data /= std
 
-    masks = np.load('numpy_data/masks.npy')
+    masks = np.load(masks_numpy_file)
     masks = masks.astype('float32')
 
     model = get_model(crop_shape)
     model.compile(optimizer=Adam(lr=learning_rate), loss=dice_coef_loss,
                   metrics=[dice_coef, recall, f1_score])
 
-    # model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
-    # model.fit(imgs_train, masks_train, batch_size=batch_size, epochs=epochs, verbose=1, shuffle=True,
-    #           callbacks=[model_checkpoint], validation_data=(imgs_test, masks_test))
-    model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
+    model_checkpoint = ModelCheckpoint(outdir+'/weights.h5', monitor='val_loss', save_best_only=True)
     model.fit(data, masks, batch_size=batch_size, epochs=epochs, verbose=1, shuffle=True,
               callbacks=[model_checkpoint],
               validation_split=0.2)
